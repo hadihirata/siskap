d2c8b57d63cbe00760f30a40dc889ada50e76d51
hadihirata <hadihirata@gmail.com>
2019-09-06 11:08:44 +0700

    Menambahkan Mingrate work

---
+++ database/migrations/2019_09_06_035732_create_works_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama');
+            $table->string('sumber');
+            $table->date('tgl');
+            $table->string('jenis_pekerjaan');
+            $table->string('programer');
+            $table->longText('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
